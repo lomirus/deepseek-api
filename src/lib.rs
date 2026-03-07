368a1f691a1242cb9dc243cf3c92bd32e110fe0e
mirus <lomirus.dev@protonmail.com>
2026-03-07 12:00:33 +0800

    Fix doc comment

--- src/lib.rs
+++ src/lib.rs
@@ -68,7 +68,7 @@ pub struct Client {
 
     /// An alternative to sampling with temperature, called nucleus sampling, where the model considers the results of the tokens with top_p probability mass. So 0.1 means only the tokens comprising the top 10% probability mass are considered.
     ///
-    /// We generally recommend altering this or `temperature`` but not both.
+    /// We generally recommend altering this or `temperature` but not both.
     pub top_p: f32,
 
     pub context: Vec<http::request::message::Message>,
