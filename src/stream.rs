e2372943ed6f3daeb3578e7cc39e81e5e8573d01
mirus <lomirus.dev@protonmail.com>
2026-04-18 14:43:31 +0800

    Fix parse_sse_data_frames error

--- src/stream.rs
+++ src/stream.rs
@@ -14,6 +14,7 @@ impl<T: AsyncIterator> AsyncIteratorNext for T {}
 pub(crate) fn parse_sse_data_frames(buf: &str) -> impl Iterator<Item = &str> {
     buf.trim()
         .split("\n\n")
+        .filter(|&frame| frame != "")
         .map(|frame| frame.strip_prefix("data: ").unwrap())
         .take_while(|data| *data != "[DONE]")
 }
